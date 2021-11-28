234410dd65ea398734e1cc3150fb97b6fc96c11b
Caleb Carlson <caleb.carlson@hpe.com>
2021-11-28 13:23:20 -0700

    caleb: Group by year for mpb

--- src/main/scala/org/cs555/MountainPineBeetleProcessing.scala
+++ src/main/scala/org/cs555/MountainPineBeetleProcessing.scala
@@ -1,12 +1,18 @@
 package org.cs555
 
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import com.mongodb.spark.config._
+import com.mongodb.spark.MongoSpark
+import org.apache.spark.sql.functions.col
 
 class MountainPineBeetleProcessing(sparkSession: SparkSession) {
 
   def processMpbData(): Unit = {
+    val readConfig: ReadConfig = ReadConfig(Map("collection" -> "mpb_cypress_hill_sk_100m"), Some(ReadConfig(sparkSession)))
+    val mpbDf: DataFrame = MongoSpark.load(sparkSession, readConfig)
 
+    val mpbPerYearDf: DataFrame = mpbDf.groupBy("YEAR").sum("MPB")
+
+    mpbPerYearDf.show(10)
   }
-
-
 }
