05cc3a588222e194578deebfa102a4300f3ef1fc
Caleb <ccarlson355@gmail.com>
2021-11-28 12:46:30 -0700

    k

--- submit.sh
+++ submit.sh
@@ -8,12 +8,8 @@ function print_usage {
 if [[ $# -eq 0 ]]; then
   echo -e "Submitting Spark Job...\n"
   ${SPARK_HOME}/bin/spark-submit \
-    --class Application \
-    --master spark://char:30633 \
-    --deploy-mode cluster \
-    --driver-memory 4g \
-    --executor-memory 2g \
-    --executor-cores 1 \
+    --class org.cs555.Application \
+    --master "local[8]" \
     target/scala-2.12/climate-analytics_2.12-0.1.jar
 else
   print_usage
