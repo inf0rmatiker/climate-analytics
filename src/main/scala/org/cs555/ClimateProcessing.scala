5f9a144beb52e80d0fea07bdbb599ac971e17d79
Caleb Carlson <caleb.carlson@hpe.com>
2021-11-28 17:36:32 -0700

    caleb convert dates to timestamps,sort

--- src/main/scala/org/cs555/ClimateProcessing.scala
+++ src/main/scala/org/cs555/ClimateProcessing.scala
@@ -1,11 +1,27 @@
 package org.cs555
 
-import org.apache.spark.sql.SparkSession
+import com.mongodb.spark.MongoSpark
+import com.mongodb.spark.config.ReadConfig
+import org.apache.spark.sql.{DataFrame, SparkSession}
+
+import java.text.SimpleDateFormat
+import java.util.Date
 
 class ClimateProcessing(sparkSession: SparkSession) {
 
   def processClimateData(): Unit = {
+    val readConfig: ReadConfig = ReadConfig(Map("collection" -> "climate_cypress_hill_sk"), Some(ReadConfig(sparkSession)))
+    val climateDf: DataFrame = MongoSpark.load(sparkSession, readConfig)
 
+    import sparkSession.implicits._
+    val filteredDf: DataFrame = climateDf.select("YEAR_MONTH_DAY", "TOTAL_SNOW_CM")
+    val mappedDatesDf = filteredDf.map(row => {
+      val dateFormat: String = "yyyy-MM-dd"
+      val simpleDateFormat: SimpleDateFormat = new SimpleDateFormat(dateFormat)
+      val parsedDate: Date = simpleDateFormat.parse(row.getString(0))
+      val timestamp: Long = parsedDate.getTime
+      (timestamp, row.getInt(1))
+    }).toDF("timestamp", "total_snow_cm").sort("timestamp")
+    mappedDatesDf.show(100)
   }
-
 }
