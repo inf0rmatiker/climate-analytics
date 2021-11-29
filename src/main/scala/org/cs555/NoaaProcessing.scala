5bd630a9c93142ed441a295f6de893e66293ad8e
Menuka Warushavithana <menuka.warushavithana@gmail.com>
2021-11-29 00:50:30 -0700

    noaa: calculate daily average values

--- src/main/scala/org/cs555/NoaaProcessing.scala
+++ src/main/scala/org/cs555/NoaaProcessing.scala
@@ -6,6 +6,12 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 class NoaaProcessing(sparkSession: SparkSession) {
   def processNoaaData(): Unit = {
+    processNoaaData("G0800510")
+  }
+
+  def processNoaaData(gisJoin: String): Unit = {
+    import sparkSession.sqlContext.implicits._
+    println(s"Processing for GISJoin: $gisJoin")
     val readConfig: ReadConfig = ReadConfig(Map("collection"-> "noaa_nam"),
       Some(ReadConfig(sparkSession)))
     val noaaDf: DataFrame = MongoSpark.load(sparkSession, readConfig)
@@ -14,10 +20,21 @@ class NoaaProcessing(sparkSession: SparkSession) {
         "YYYYMMDDHHHH",
         "SNOW_COVER_AT_SURFACE_PERCENT",
         "DEWPOINT_TEMPERATURE_2_METERS_ABOVE_SURFACE_KELVIN",
-        "TEMPERATURE_AT_SURFACE_KELVIN"
+        "TEMPERATURE_AT_SURFACE_KELVIN",
+        "RELATIVE_HUMIDITY_2_METERS_ABOVE_SURFACE_PERCENT"
       )
+      .filter($"GISJOIN".contains(gisJoin))
+      .withColumn("year_month_day", $"YYYYMMDDHHHH".substr(0, 8))
+      .groupBy("year_month_day")
+      .mean(
+        "SNOW_COVER_AT_SURFACE_PERCENT",
+        "DEWPOINT_TEMPERATURE_2_METERS_ABOVE_SURFACE_KELVIN",
+        "TEMPERATURE_AT_SURFACE_KELVIN",
+        "RELATIVE_HUMIDITY_2_METERS_ABOVE_SURFACE_PERCENT"
+    )
 
     noaaDf.show(5)
+    println(s"#rows = ${noaaDf.count()}")
 
     val saver: DataFrameSaver = new DataFrameSaver(".")
 
